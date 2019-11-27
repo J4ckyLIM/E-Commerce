5f66125be2c2ea77bb01d1798f78464017c558fa
Unknown <riyad.achour@edu.itescia.fr>
2019-11-27 09:20:57 +0100

    Login / register _ Firebase

--- e_commerce/lib/main.dart
+++ e_commerce/lib/main.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'Task.dart';
+import 'RegistrerPage.dart';
+import 'SplashPage.dart';
+import 'LoginPage.dart';
+import 'home.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('App Test'),
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: TextField(
-        decoration: InputDecoration(
-          border: InputBorder.none,
-          hintText: 'Enter a search term',
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
         ),
-      ),
-    ),
-  ),
-));
+        home: SplashPage(),
+        routes: <String, WidgetBuilder>{
+          '/task': (BuildContext context) => TaskPage(title: 'Task'),
+          '/home': (BuildContext context) => HomePage(title: 'Home'),
+          '/login': (BuildContext context) => LoginPage(),
+          '/register': (BuildContext context) => RegisterPage(),
+        });
+  }
+}
